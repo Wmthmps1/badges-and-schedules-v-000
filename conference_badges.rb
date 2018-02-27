518bade6a45a63eb7ed3470681f928d91b70576e
William Thompson <wmthmps1@gmail.com>
2018-02-27 18:11:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  new_array []
+  new_array = []
    attendees.each do |names|
      new_array << "Hello, my name is #{names}"
    end
